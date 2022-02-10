1a60e4b72f9aab44484e86fd7d0dd9f5fa2b5808
evelina.tubalets <evelina.tubalec@gmail.com>
2022-02-10 18:55:49 +0300

    Initial commit

---
+++ src/main/java/com/example/jenkinstester/JenkinsTesterApplication.java
@@ -0,0 +1,15 @@
+package com.example.jenkinstester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTesterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JenkinsTesterApplication.class, args);
+        System.out.println("hi");
+    }
+
+}
